dbbcb2b10089cd480ebfd3870fc369e583e2686f
kmsulekha <91432258+kmsulekha@users.noreply.github.com>
2021-09-27 01:03:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "launching_os" {
-source = "github.com/kmsatyam/instance_ebs_volume_resources?ref=v0.0.1"
+source = "github.com/kmsulekha/aws_instance_ebs_resource?ref=v0.1"
 instance_type                 = var.instance_type
 availability_zone             = var.availability_zone
 vpc_security_group_ids        = var.vpc_security_group_ids
